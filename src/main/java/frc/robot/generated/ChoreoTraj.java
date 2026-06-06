e8f39eb0b78b8f6a9ab29a2c3105c9d1315aebf4
CJendantix <cjendantix@proton.me>
2026-06-05 20:15:29 -0500

    Update path

--- src/main/java/frc/robot/generated/ChoreoTraj.java
+++ src/main/java/frc/robot/generated/ChoreoTraj.java
@@ -38,7 +38,7 @@ public record ChoreoTraj(
     public static final ChoreoTraj Neutral2 = new ChoreoTraj(
         "Neutral2",
         OptionalInt.empty(),
-        5.73358,
+        5.91726,
         new Pose2d(3.12514, 7.30892, Rotation2d.fromRadians(-1.15368)),
         new Pose2d(3.12514, 7.30892, Rotation2d.fromRadians(-1.15368))
     );
